57b97b3cab07d2c39648d3b5d945351c9f98b2ca
Jhoan Cevallos <jhoancevallosvillavicencio@gmail.com>
2026-04-20 07:50:43 -0500

    ACTUALIZACION DE CUENTA BANCARIA CON VISUALIZACION DE CUENTAS

--- Ejemplos completos/Cuenta_Bancaria_con_privados.py
+++ Ejemplos completos/Cuenta_Bancaria_con_privados.py
@@ -28,8 +28,8 @@ class Cuenta_Bancaria:
         if self.__saldo_inicial<saldo_a_retirar:
             print(f"Retiro cancelado por falta de fondos")
         else:
-            self._saldo__inicial-=saldo_a_retirar
-            print(f"Retiro realizado su saldo actual es {self.__saldo_inicial} por falta de fondos")
+            self.__saldo_inicial-=saldo_a_retirar
+            print(f"Retiro realizado su saldo actual es {self.__saldo_inicial} ")
 
     
     def versaldo(self):
@@ -37,19 +37,42 @@ class Cuenta_Bancaria:
 
              
 def guardarcuentas (cuenta):
-    cuentasidc={
-        "Nombre dle Titualar": cuenta.nombre,
-        "Saldo Actial": cuenta.saldo
-     }
-    with open("cuentas_y_saldos.json","w") as archivo:
-            json.dump(cuentasidc, archivo, indent=4)
+    nombre_archivo = "cuentas_y_saldos.json"
+    try:
+        with open(nombre_archivo, "r") as archivo:
+            historial_cuentas=json.load(archivo)
+    except FileNotFoundError:
+        historial_cuentas = {}
+    
+    historial_cuentas[cuenta.nombre] = {
+        "Saldo": cuenta.saldo
+    }
+    
+    with open(nombre_archivo, "w") as archivo:
+        json.dump(historial_cuentas, archivo, indent=4)
+        print(f"Datos de {cuenta.nombre} guradado correctamente ")
 
 
-
+def ver_vuentas():
+    try:
+        nombre_archivo = "cuentas_y_saldos.json"
+        with open(nombre_archivo, "r") as archivo:
+            historial=json.load(archivo)
+            print("\n--- LISTA DE CUENTAS EN EL BANCO ---")
+            for nombre, datos in historial.items():
+                print(f"Nombre: {nombre} | Saldo: {datos['Saldo']}")
+    except FileNotFoundError:
+        print ("Aún no hay cuentas registradas en el sistema.")
+        
+        
 
 cuenta_jhoan = Cuenta_Bancaria("Jhoan", 500)
-cuenta_jhoan._saldo_inicial=100000
-cuenta_jhoan.retiro(800)
-
-
+cuenta_jhoan.retiro(100) 
 guardarcuentas(cuenta_jhoan)
+
+cuenta_papa = Cuenta_Bancaria("Papa", 2000)
+cuenta_papa.depostiar(500) 
+guardarcuentas(cuenta_papa)
+
+# Llamamos a la función para ver toda la base de datos
+ver_vuentas()
